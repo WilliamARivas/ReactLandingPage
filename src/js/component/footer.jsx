f7fbf03f4bc4f44af974cdc5e186c8835b8ab512
WilliamARivas <willarivas@yahoo.com>
2022-10-05 17:56:44 +0000

    save progress

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MyFooter = () => {
+
+    return(
+        <>
+        <footer className="bg-dark text-center fixed-bottom">
+            <div className="text-center p-3 text-light">
+                © 2020 Copyright:
+                <a className="text-light" href="https://mdbootstrap.com/">YourWebsite.com</a>
+            </div>
+        </footer>
+        </>    
+    )
+};
+
+export default MyFooter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import your own components
+import MyNavBar from "./navbar.jsx";
+import MyJumboTron from "./jumbotron.jsx";
+import MyFooter from "./footer.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<MyNavBar />
+		<div className="container">
+			<div className="row">
+				<MyJumboTron />
+			</div>
+		
+			<br></br>
+
+			<div className="row justify-content-around" id="cards">
+				<div className="col-2">1</div>
+				<div className="col-2">2</div>
+				<div className="col-2">3</div>
+				<div className="col-2">4</div>		
+			</div>
 		</div>
+		<MyFooter/>	
+		</>
 	);
 };
 
